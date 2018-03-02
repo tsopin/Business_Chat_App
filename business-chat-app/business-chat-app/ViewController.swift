cb4bbcf93444b88851a77208486ba784c360da09
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2018-03-02 00:46:50 -0500

    init project

---
+++ business-chat-app/business-chat-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  business-chat-app
+//
+//  Created by Viktor Bilyk on 2018-03-02.
+//  Copyright © 2018 Brogrammers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
